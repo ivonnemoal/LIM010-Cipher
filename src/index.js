bec69d521edfabbece979fe57b68298f68032cce
ivonnemoal <ivonne.morales.alarcon@gmail.com>
2019-06-07 11:15:44 -0500

    Añadiendo carpeta imgReadme

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 /* Acá va tu código */
-const password = document.getElementById('password').value;
-const name = document.getElementById('name').value;
+const password = document.getElementById('password');
+const name = document.getElementById('name');
 const textResult = document.getElementById('result');
 const btnLogin = document.getElementById('btn');
 let counter = 3;
 
-btnLogin.addEventListener('click', () => {
+const validatePassword = btnLogin.addEventListener('click', () => {
     counter = counter - 1;
-    console.log(counter);
     if(counter === 0) {
         textResult.innerHTML = '¡YOU CAN NOT ENTER FOR NOW!';
-    }else if(password === 'LABORATORIA'){
-        textResult.innerHTML = 'WELCOME'+ ' ' + name;
+    }else if(password.value === 'LABORATORIA'){
+        textResult.innerHTML = 'WELCOME'+ ' ' + name.value;
     }else {
         textResult.innerHTML = 'incorrect password try again';
     }
 });
+
 
 // const clavecita = btn.addEventListener('click',incorrecta());
 // // p.innerHTML = clavecita;
