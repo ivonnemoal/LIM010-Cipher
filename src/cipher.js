5545b207419c6a098811999df64b95ce358bf07c
ivonnemoal <ivonne.morales.alarcon@gmail.com>
2019-06-12 10:53:49 -0500

    Readme Terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,22 @@
- window.cipher = {
+window.cipher = {
 
-encode: (offset, string) => {
-  let textResult = '';
-  for (let i = 0; i < string.length; i++) {
-    const Ascii = string.charCodeAt(i);
-    let cipherCode = (Ascii - 65 + offset) % 26 + 65;
-    const letterEncode = String.fromCharCode(cipherCode);
-    textResult += letterEncode;
-  } return textResult;
-},
+  encode: (offset, string) => {
+    let textResult = '';
+    for (let i = 0; i < string.length; i++) {
+      const Ascii = string.charCodeAt(i);
+      let cipherCode = (Ascii - 65 + offset) % 26 + 65;
+      const letterEncode = String.fromCharCode(cipherCode);
+      textResult += letterEncode;
+    } return textResult;
+  },
 
-decode: (offset, string) => {
-  let textResult = '';
-  for (let i = 0; i < string.length; i++) {
-    const Ascii = string.charCodeAt(i);
-    let cipherCode = (Ascii - 65 - offset) % 26 + 65;
-    const letterDecode = String.fromCharCode(cipherCode);
-    textResult += letterDecode;
-  } return textResult;
-}
+  decode: (offset, string) => {
+    let textResult = '';
+    for (let i = 0; i < string.length; i++) {
+      const Ascii = string.charCodeAt(i);
+      let cipherCode = (Ascii - 65 - offset) % 26 + 65;
+      const letterDecode = String.fromCharCode(cipherCode);
+      textResult += letterDecode;
+    } return textResult;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-/* Acá va tu código */
-
-// const result = document.getElementById('result');
 const welcome = document.getElementById('welcome');
 const locked = document.getElementById('locked');
 const error = document.getElementById('error');
-const btnLogin = document.getElementById('btn');
+const btnLogin = document.getElementById('btnLogin');
 const btnEncode = document.getElementById('btnEncode');
 const btnDecode = document.getElementById('btnDecode');
 let counter = 2;
 
-// funcion de ingreso
+// Logeo
 const validatePassword = btnLogin.addEventListener('click', () => {
     const name = document.getElementById('name').value;
     document.getElementById('name').value = '';
@@ -21,33 +18,46 @@ const validatePassword = btnLogin.addEventListener('click', () => {
         counter--;
         if (password === 'LABORATORIA') {
             welcome.innerHTML = 'Welcome' + ' ' + name;
-        }else {
-            error.innerHTML = 'incorrect password try again';}
-    }else {
-        locked.innerHTML = '¡WE ARE SORRY BUT YOU CAN NOT ENTER FOR NOW!';}
+        } else {
+            error.innerHTML = 'incorrect password try again';
+        }
+    } else {
+        locked.innerHTML = '¡WE ARE SORRY BUT YOU CAN NOT ENTER FOR NOW!';
+    }
 });
 
-const Encode = btnEncode.addEventListener('click',(e) => {
+const Encode =
+
+    btnEncode.addEventListener('click', (e) => {
+        const offset = parseInt(document.getElementById('offset').value);
+        document.getElementById('offset').value = '';
+
+        if (e) {
+            const string = document.getElementById('textCode').value;
+            document.getElementById('textCode').value = '';
+
+            const result = document.getElementById('result');
+            result.innerHTML = cipher.encode(offset, string);
+        }
+    });
+
+const Decode = btnDecode.addEventListener('click', (e) => {
     const offset = parseInt(document.getElementById('offset').value);
     document.getElementById('offset').value = '';
-  
-if(e){
-    const string = document.getElementById('textCode').value;
-    document.getElementById('textCode').value = '';
+    if (e) {
+        const string = document.getElementById('textCode').value;
+        document.getElementById('textCode').value = '';
 
-    const result = document.getElementById('result');
-    result.innerHTML = cipher.encode(offset,string);   
+        const result = document.getElementById('result');
+        result.innerHTML = cipher.decode(offset, string);
     }
 });
 
-const Decode = btnDecode.addEventListener('click',(e) => {
-    const offset = parseInt(document.getElementById('offset').value);
-    
-    if(e){
-    const string = document.getElementById('textCode').value;
-    document.getElementById('textCode').value = '';
 
-    const result = document.getElementById('result');
-    result.innerHTML = cipher.decode(offset,string);
-    }
-});
+// Reset
+// const btnReset = document.getElementById('btnReset');
+
+// const Reset = btnReset.addEventListener('click',() => {
+
+// });
+
