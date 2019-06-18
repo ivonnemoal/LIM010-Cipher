52fb9c37fef3d8e31ef9f0ae069f5054cdb9f085
ivonnemoal <ivonne.morales.alarcon@gmail.com>
2019-06-18 10:09:48 -0500

    Modificando funcion caracteres

--- src/cipher.js
+++ src/cipher.js
@@ -16,13 +16,19 @@ window.cipher = {
       } else if (Ascii >= 48 && Ascii <= 57 && offset >= 0) {
         let cipherCode = (Ascii - 48 + offset) % 10 + 48;
         letterEncode = String.fromCharCode(cipherCode);
-      } else if (Ascii >= 33 && Ascii <= 43 && offset >= 0) {
-        let cipherCode = (Ascii - 33 + offset) % 10 + 33;
+      } else if (Ascii >= 33 && Ascii <= 47 && offset >= 0) {
+        let cipherCode = (Ascii - 33 + offset) % 15 + 33;
         letterEncode = String.fromCharCode(cipherCode);
-      } else if (Ascii >= 128 && Ascii <= 165 && offset >= 0) {
-        let cipherCode = (Ascii - 128 + offset) % 38 + 128;
+      } else if (Ascii >= 58 && Ascii <= 64 && offset >= 0) {
+        let cipherCode = (Ascii - 58 + offset) % 7  + 58;
         letterEncode = String.fromCharCode(cipherCode);
-      } else{
+      } else if (Ascii >= 91 && Ascii <= 96 && offset >= 0) {
+        let cipherCode = (Ascii - 91 + offset) % 6  + 91;
+        letterEncode = String.fromCharCode(cipherCode);
+      } else if (Ascii >= 123 && Ascii <= 255 && offset >= 0) {
+        let cipherCode = (Ascii - 123 + offset) % 133  + 123;
+        letterEncode = String.fromCharCode(cipherCode);
+      }else{
         let otherCaracter = String.fromCharCode(Ascii);
         letterEncode = otherCaracter;
       } textResult += letterEncode;
@@ -45,11 +51,17 @@ window.cipher = {
       } else if (Ascii >= 48 && Ascii <= 57 && offset >= 0) {
         let cipherCode = (Ascii - 57 - offset) % 10 + 57;
         letterEncode = String.fromCharCode(cipherCode);
-      } else if (Ascii >= 33 && Ascii <= 43 && offset >= 0) {
-        let cipherCode = (Ascii - 43 - offset) % 10 + 43;
+      } else if (Ascii >= 33 && Ascii <= 47 && offset >= 0) {
+        let cipherCode = (Ascii - 47 - offset) % 15 + 47;
         letterEncode = String.fromCharCode(cipherCode);
-      } else if (Ascii >= 128 && Ascii <= 165 && offset >= 0) {
-        let cipherCode = (Ascii - 165 - offset) % 110 + 165;
+      } else if (Ascii >= 58 && Ascii <= 64 && offset >= 0) {
+        let cipherCode = (Ascii - 64 - offset) % 7  + 64;
+        letterEncode = String.fromCharCode(cipherCode);
+      } else if (Ascii >= 91 && Ascii <= 96 && offset >= 0) {
+        let cipherCode = (Ascii - 96 - offset) % 6  + 96;
+        letterEncode = String.fromCharCode(cipherCode);
+      } else if (Ascii >= 123 && Ascii <= 255 && offset >= 0) {
+        let cipherCode = (Ascii - 255 - offset) % 133  + 255;
         letterEncode = String.fromCharCode(cipherCode);
       } else {
         let otherCaracter = String.fromCharCode(Ascii);
